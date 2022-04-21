98a0bc2fa7a1aa23f679503513710c30b0b111ab
Jorge <jorgeharanda97@yahoo.com>
2022-04-20 17:51:38 -0700

    Fixed Collision, Setup first level
    Fixed collision with walls, set up the main scene to setup first level. Need to setup logic now

--- Paddle.gd
+++ Paddle.gd
@@ -1,16 +1,16 @@
 extends KinematicBody2D
 
 var score : int = 0
-var speed : int = 200
+var speed : int = 350
 var vel : Vector2 = Vector2()
 onready var sprite : Sprite = get_node("Paddle")
 
 func _physics_process(delta):
 	vel.x = 0
 	
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left_arrow") or Input.is_action_pressed("move_left_a"):
 		vel.x -= speed
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right_arrow") or Input.is_action_pressed("move_right_d"):
 		vel.x += speed
 	
 	vel = move_and_slide(vel, Vector2.UP)
