07a3da0a35e9c0c287e18d935582be119fa32b52
aslee28 <81182187+aslee28@users.noreply.github.com>
2022-05-11 18:43:29 -0700

    Delete VariantGame directory

--- VariantGame/logic/AddBalKinematic.gd
+++
@@ -1,5 +0,0 @@
-extends KinematicBody2D
-
-func add_ball_hit():
-	print("Add Ball Power Obtained!")
-	get_parent().queue_free()
--- VariantGame/logic/SpeedUpKinematic.gd
+++
@@ -1,5 +0,0 @@
-extends KinematicBody2D
-
-func speed_up_hit():
-	print("Speed Up Power Obtained!")
-	get_parent().queue_free()
